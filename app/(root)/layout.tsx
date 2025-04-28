38b65ee0885f313ad9355b68966d6f29b01388cc
Jomar <jomarhan.ng@gmail.com>
2025-04-28 11:22:08 -0400

    config tailwind and shadcn

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="font-sans">
+    <main className="font-work-sans">
       <Navbar />
       {children}
     </main>
